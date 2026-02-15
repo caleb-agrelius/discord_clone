e60320d1e94e9a2f9f55392e9b8995fdee8701b8
calebagrelius <calebagrelius@gmail.com>
2026-02-15 10:28:41 -0800

    pages update

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import SignIn from "./pages/SignIn.tsx";
+import FriendsList from "./pages/FriendsList.tsx";
+
 import UserSidebar from "./components/UserSidebar.tsx";
-import TextChat from "./components/TextChat.tsx";
 import TavernTables from "./components/TavernTables.tsx";
 
 import { DEV_BYPASS } from "./util/constants.ts";
@@ -10,6 +11,7 @@ import { FaUserFriends, FaHouseUser } from "react-icons/fa";
 
 export default function App() {
     const [userSignedIn, setUserSignedIn] = React.useState(DEV_BYPASS);
+    const [page, setPage] = React.useState("Taverns");
 
     if (!userSignedIn) {
         return (
@@ -24,15 +26,15 @@ export default function App() {
             <div className="header">
                 <p>The Yawning Portal</p>
                 <div className="header-buttons">
-                <button><FaHouseUser /></button>
-                <button><FaUserFriends /></button>
+                <button onClick={() => setPage("Taverns")}><FaHouseUser /></button>
+                <button onClick={() => setPage("FriendsList")} ><FaUserFriends /></button>
                 </div>
             </div>
             <div className="main-layout">
                 
                 <div className="main-content">
-                    {page === "Taverns" ? <TavernTables /> : <TextChat />}
-                    
+                    {page === "Taverns" && <TavernTables />}
+                    {page === "FriendsList" && <FriendsList />}
                 </div>
                 <UserSidebar />
             </div>
